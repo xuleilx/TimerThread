da6a900ed893caeee9dd69fd77ddc1f83136add3
徐磊 <xuleilx@gmail.com>
2018-10-16 15:18:13 +0800

    Add files via upload
    First Release

---
+++ Thread.cpp
@@ -0,0 +1,35 @@
+#include "Thread.h"
+
+Thread::Thread(ThreadCallback cb)
+    :_threadId(0),
+     _isRun(false),
+     _callback(cb)
+{
+
+}
+
+Thread::~Thread()
+{
+    if(_isRun)
+        pthread_detach(_threadId);
+}
+
+
+void Thread::start()
+{
+    pthread_create(&_threadId, NULL, runInThread, this);
+    _isRun = true;
+}
+
+void Thread::join()
+{
+    pthread_join(_threadId, NULL);
+    _isRun = false;
+}
+
+void *Thread::runInThread(void *arg)
+{
+    Thread *p = static_cast<Thread *>(arg);
+    p->_callback();
+    return NULL;
+}
